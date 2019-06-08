b844ac32324ef6a150bcb0979a024e08084bb83f
Orivaldo Santana <orivaldo@gmail.com>
2019-06-08 15:23:59 -0300

    rota de autenticação com verificação de usuário válido e senha

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs'); 
 
 const User = require('../models/user');
 
@@ -17,6 +18,17 @@ router.post('/register', async (req, res) => {
         console.log(err);
         return res.status(400).send({ error: 'Registration Failed' });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body; 
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user)
+        return res.status(400).send({error: 'User not found'});
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error: 'Invalid password'});
+    res.send({ user }); 
 });
 
 module.exports = app => app.use('/auth', router); 
